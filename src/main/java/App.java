7441c20d7a1be9bf29e25acb848c14ccdac773e9
Sara Johnson <johzsara@amazon.com>
2020-12-22 16:19:23 -0800

    TwoSum Brute Force

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,21 +1,33 @@
 import ExerciseClasses.KeywordSuggestions;
+import ExerciseClasses.TwoSum;
+
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("LeetCode Practice:");
+        System.out.println();
 
+        System.out.println("Problem: Keyword Suggestions");
         String[] myProducts = new String[]{"bags","baggage","banner","box","cloths"};
         String myWord = "bags";
 
         String[] myProducts2 = new String[]{"mobile","mouse","moneypot","monitor","mousepad"};
         String myWord2 = "mouse";
 
-
-
         KeywordSuggestions suggestions = new KeywordSuggestions();
         System.out.println(suggestions.suggestedProducts(myProducts2, myWord2));
+        System.out.println();
 
+        System.out.println("Problem: Two Sum");
+        int[] arr1 = new int[]{2,7,11,15};
+        int target1 = 9;
 
+        int[] arr2 = new int[]{3,2,4};
+        int target2 = 6;
+
+        TwoSum twoSum = new TwoSum();
+        System.out.println(Arrays.toString(twoSum.twoSum(arr2, target2)));
 
     }
 }
---
+++ src/main/java/ExerciseClasses/TwoSum.java
@@ -0,0 +1,26 @@
+package ExerciseClasses;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ * https://leetcode.com/problems/two-sum/
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    return new int[] {i,j};
+                }
+            }
+        }
+
+        return null;
+    }
+
+}
