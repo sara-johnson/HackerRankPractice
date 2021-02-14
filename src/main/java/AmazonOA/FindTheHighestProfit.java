9e58f7f39fe9a0c1aca9547316281d209d6df089
Sara Johnson <johzsara@amazon.com>
2021-02-14 15:43:40 -0800

    Find the HighestProfit with Priority Queue

--- src/main/java/AmazonOA/FindTheHighestProfit.java
+++ src/main/java/AmazonOA/FindTheHighestProfit.java
@@ -1,4 +1,41 @@
 package AmazonOA;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class FindTheHighestProfit {
+
+    public static int findProfit(int numSuppliers, List<Integer> inventory, int order) {
+
+        int totalProfit = 0;
+
+        Collections.sort(inventory);
+        Collections.reverse(inventory);
+
+        System.out.println(inventory);
+
+        PriorityQueue<Integer> profits = new PriorityQueue<>(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer i1 , Integer i2) {
+                if (i1 > i2) return -1;
+                return 1;
+            }
+        });
+        for (int i = 0; i < numSuppliers; i++) {
+            int size = inventory.get(i);
+            for (int j = 0; j < inventory.get(i); j++) {
+                profits.add(size);
+                size--;
+            }
+        }
+
+        while (!profits.isEmpty() && order > 0) {
+            totalProfit += profits.remove();
+            order--;
+        }
+
+        return totalProfit;
+    }
 }
--- src/main/java/ExerciseClasses/OAApp.java
+++ src/main/java/ExerciseClasses/OAApp.java
@@ -1,2 +1,27 @@
-package ExerciseClasses;public class OAApp {
+package ExerciseClasses;
+
+import AmazonOA.FindTheHighestProfit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OAApp {
+    public static void main(String[] args) {
+        System.out.println("LeetCode Practice:");
+        System.out.println();
+
+        System.out.println("Problem: Find The Highest Profit");
+        int numSuppliers1 = 5;
+        List<Integer> inventory1 = new ArrayList<>();
+        inventory1.add(2);
+        inventory1.add(8);
+        inventory1.add(4);
+        inventory1.add(10);
+        inventory1.add(6);
+        int order1 = 20;
+
+        System.out.println(FindTheHighestProfit.findProfit(numSuppliers1, inventory1, order1));
+        System.out.println();
+    }
+
 }
