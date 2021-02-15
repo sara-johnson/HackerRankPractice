74f747260a3cdbc5df9e45cbf29cc8c844bbce37
Sara Johnson <johzsara@amazon.com>
2021-02-14 18:21:10 -0800

    Maximum Disk Space with a Priority queue

--- src/main/java/AmazonOA/MaxDiskSpace.java
+++ src/main/java/AmazonOA/MaxDiskSpace.java
@@ -1,4 +1,51 @@
 package AmazonOA;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class MaxDiskSpace {
+
+    public int available_space(int numComputer, List<Integer> hardDiskSpace, int segmentLength) {
+
+        int startIndex = 0;
+        int endIndex = startIndex + segmentLength;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+
+                if (o1 < o2) {
+                    return 1;
+                }
+                if (o1 > o2) {
+                    return -1;
+                }
+                return 0;
+                //return o2.compareTo(o1); <same as above if statements>
+
+            }
+        });
+
+        List<Integer> subArray;
+
+        //loop through the entire array
+
+        while (startIndex < endIndex && endIndex < numComputer) {
+            int min = Integer.MAX_VALUE;
+            subArray = hardDiskSpace.subList(startIndex, endIndex);
+
+            for (Integer hd : subArray) {
+                if (hd < min) {
+                    min = hd;
+                }
+            }
+            System.out.println("Min: " + min);
+            pq.add(min);
+            startIndex++;
+            endIndex++;
+        }
+
+        return pq.remove();
+    }
 }
--- src/main/java/ExerciseClasses/OAApp.java
+++ src/main/java/ExerciseClasses/OAApp.java
@@ -1,6 +1,7 @@
 package ExerciseClasses;
 
 import AmazonOA.FindTheHighestProfit;
+import AmazonOA.MaxDiskSpace;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,33 @@ public class OAApp {
 
         System.out.println(FindTheHighestProfit.findProfit(numSuppliers1, inventory1, order1));
         System.out.println();
+        System.out.println();
+
+        System.out.println("Problem: Max Disk Space");
+        int numComputer1 = 3;
+        List<Integer> hardDiskSpace1 = new ArrayList<>();
+        hardDiskSpace1.add(8);
+        hardDiskSpace1.add(2);
+        hardDiskSpace1.add(4);
+        int segmentLength1 = 2; //answer is 2
+
+        int numComputer2 = 10;
+        List<Integer> hardDiskSpace2 = new ArrayList<>();
+        hardDiskSpace2.add(8);
+        hardDiskSpace2.add(2);
+        hardDiskSpace2.add(7);
+        hardDiskSpace2.add(4);
+        hardDiskSpace2.add(5);
+        hardDiskSpace2.add(4);
+        hardDiskSpace2.add(9);
+        hardDiskSpace2.add(4);
+        hardDiskSpace2.add(6);
+        hardDiskSpace2.add(3);
+        int segmentLength2 = 3;  //answer is 4
+
+        MaxDiskSpace maxDiskSpace = new MaxDiskSpace();
+        System.out.println(maxDiskSpace.available_space(numComputer2, hardDiskSpace2, segmentLength2));
+
     }
 
 }
